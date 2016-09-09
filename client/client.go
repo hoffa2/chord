b64a61d69ec1a3b4645bf1a7099392cb97d41b72
hoffa2 <helhof@online.no>
2016-09-09 16:24:16 +0200

    feat: ipAdresses getter for client

--- client/client.go
+++ client/client.go
@@ -1,12 +1,35 @@
 package client
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 type Client struct {
-	IPs            []string
-	NameServerAddr string
+	IPs        []string
+	nameServer string
+	conn       http.Client
 }
 
-func getIPAddrs() {
+func (c *Client) getIPAddrs() {
+	req, err := http.NewRequest("GET", c.nameServer, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := c.conn.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("could not retrieve ipadresses from nameserver: %s", c.nameServer)
+	}
+
+	err := json.NewDecoder(resb.Body).Decode(c.IPs)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func putKey() {
